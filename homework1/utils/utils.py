c336c0b82cd0cd231034d219fb16aaead7655980
Huy <nguye2cy@mail.uc.edu>
2024-09-22 18:29:14 -0400

    Apply alphabetical order for adjacency list

--- homework1/utils/utils.py
+++ homework1/utils/utils.py
@@ -3,25 +3,25 @@ import time
 # Romania road map as adjacency list
 romania_map = {
     'Arad': [('Zerind', 75), ('Sibiu', 140), ('Timisoara', 118)],
-    'Zerind': [('Arad', 75), ('Oradea', 71)],
-    'Oradea': [('Zerind', 71), ('Sibiu', 151)],
-    'Sibiu': [('Arad', 140), ('Oradea', 151), ('Fagaras', 99), ('Rimnicu Vilcea', 80)],
-    'Timisoara': [('Arad', 118), ('Lugoj', 111)],
+    'Bucharest': [('Fagaras', 211), ('Pitesti', 101), ('Giurgiu', 90), ('Urziceni', 85)],
+    'Craiova': [('Drobeta', 120), ('Rimnicu Vilcea', 146), ('Pitesti', 138)],
+    'Drobeta': [('Mehadia', 75), ('Craiova', 120)],
+    'Eforie': [('Hirsova', 86)],
+    'Fagaras': [('Sibiu', 99), ('Bucharest', 211)],
+    'Giurgiu': [('Bucharest', 90)],
+    'Hirsova': [('Urziceni', 98), ('Eforie', 86)],
+    'Iasi': [('Vaslui', 92), ('Neamt', 87)],
     'Lugoj': [('Timisoara', 111), ('Mehadia', 70)],
     'Mehadia': [('Lugoj', 70), ('Drobeta', 75)],
-    'Drobeta': [('Mehadia', 75), ('Craiova', 120)],
-    'Craiova': [('Drobeta', 120), ('Rimnicu Vilcea', 146), ('Pitesti', 138)],
-    'Rimnicu Vilcea': [('Sibiu', 80), ('Craiova', 146), ('Pitesti', 97)],
-    'Fagaras': [('Sibiu', 99), ('Bucharest', 211)],
+    'Neamt': [('Iasi', 87)],
+    'Oradea': [('Zerind', 71), ('Sibiu', 151)],
     'Pitesti': [('Rimnicu Vilcea', 97), ('Craiova', 138), ('Bucharest', 101)],
-    'Bucharest': [('Fagaras', 211), ('Pitesti', 101), ('Giurgiu', 90), ('Urziceni', 85)],
-    'Giurgiu': [('Bucharest', 90)],
+    'Rimnicu Vilcea': [('Sibiu', 80), ('Craiova', 146), ('Pitesti', 97)],
+    'Sibiu': [('Arad', 140), ('Oradea', 151), ('Fagaras', 99), ('Rimnicu Vilcea', 80)],
+    'Timisoara': [('Arad', 118), ('Lugoj', 111)],
     'Urziceni': [('Bucharest', 85), ('Hirsova', 98), ('Vaslui', 142)],
-    'Hirsova': [('Urziceni', 98), ('Eforie', 86)],
-    'Eforie': [('Hirsova', 86)],
     'Vaslui': [('Urziceni', 142), ('Iasi', 92)],
-    'Iasi': [('Vaslui', 92), ('Neamt', 87)],
-    'Neamt': [('Iasi', 87)]
+    'Zerind': [('Arad', 75), ('Oradea', 71)]
 }
 
 # Heuristic function: straight-line distance to Bucharest h(n)
