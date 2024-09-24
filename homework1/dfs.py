362a029acce7fd83256edb944c72f05cb285781a
ToanNham <124535051+ToanNham@users.noreply.github.com>
2024-09-23 20:23:41 -0400

    Update dfs.py
    add docstring

--- homework1/dfs.py
+++ homework1/dfs.py
@@ -2,6 +2,17 @@ from utils.utils import *
 
 @timeit(iterations=1000)
 def depth_first_search(start, goal='Bucharest'):
+    """
+    Implements the Depth-First Search (DFS) algorithm to find a path from the start city to the goal city.
+
+    Parameters:
+    - start (str): The starting city.
+    - goal (str): The goal city. Defaults to 'Bucharest'.
+
+    Returns:
+    - tuple: A tuple containing the path as a list of cities and the number of nodes visited.
+             If no path is found, returns an empty list and the number of nodes visited.
+    """
     visited = set()
     stack = [start]
     parent = {start: None}
