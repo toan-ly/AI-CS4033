2f45e459c22e852654362c4ef43f68e3dc628b97
toan-ly <toanly0521@gmail.com>
2024-11-26 13:14:03 -0500

    fix time exceed, optimize code

--- homework4/BlocksWorld-Group8-FirstLast.pl
+++ homework4/BlocksWorld-Group8-FirstLast.pl
@@ -1,3 +1,15 @@
+/**
+ * Blocks World Problem 
+ * 
+ * Group 8:
+ * Huy Nguyen
+ * Toan Ly
+ * Toan Nham
+ * 
+ * All contributed equally
+ */
+
+
 % Define all the blocks in the world
 blocks([a, b, c, d, e, f]).
 
@@ -7,18 +19,22 @@ block(X) :-
     member(X, BLOCKS). % this checks if X is a member of BLOCKS
 
 % Define the start and goal states
-start([[on, a, b], [on, b, 'table'], [on, c, d], [clear, c], [clear, a], [on, d, 'table']]).
-goal([[on, d, a], [on, a, c], [on, c, b], [on, b, 'table'], [clear, a]]).
+:- dynamic(start/1).
+:- dynamic(goal/1).
 
-% notequal(X1, X2) takes 2 args and holds true when they aren't 
+% start([[on, a, b], [on, b, 'table'], [on, c, d], [clear, c], [clear, a], [on, d, 'table']]).
+% goal([[on, d, a], [on, a, c], [on, c, b], [on, b, 'table'], [clear, a]]).
+
+% notequal(X1, X2) takes 2 args and holds true when they are not
 % equal. In other words, it fails when the args are equal and otherwise succeeds
-notequal(X, Y) :- X \= Y.
 
 % notequal(X, X) :-!, fail. % fail, if equal
 % notequal(_, _). % otherwise, succeed
+notequal(X, Y) :- X \= Y.
+
 
 % substitute(E, E1, OLD, NEW) holds when NEW is the list OLD in which E is substituted 
-% by E1. There're no dups in OLD or NEW
+% by E1. There are no dups in OLD or NEW
 substitute(X, Y, [X|T], [Y|T]).
 substitute(X, Y, [H|T], [H|T1]) :-
     substitute(X, Y, T, T1).
@@ -37,7 +53,7 @@ move_to_table(X, Y, S1, S2) :-
     member([clear, X], S1), % find a clear block X in S1
     member([on, X, Y], S1), block(Y), % find a block on which X sits
     substitute([on, X, Y], [on, X, 'table'], S1, INT), % remove X from Y, place it on the table
-    append([[clear, Y]], INT, S2). % now, Y is also clear, no need to substitute
+    append(INT, [[clear, Y]], S2). % now, Y is also clear
 
 % move a block from the table onto another block
 move_from_table(X, Z, S1, S2) :-
@@ -45,7 +61,7 @@ move_from_table(X, Z, S1, S2) :-
     member([on, X, 'table'], S1), % X is on the table
     member([clear, Z], S1), notequal(X, Z), % find another clear block Z
     substitute([on, X, 'table'], [on, X, Z], S1, INT), % remove X from the table, place it on Z
-    substitute([clear, Z], [], INT, S2). % Z is no longer clear
+    delete(INT, [clear, Z], S2). % Z is no longer clear
 
 % there is a path from state S1 to S2 when there is a move from S1 to S2
 path(S1, S2) :-
@@ -59,23 +75,24 @@ path(S1, S2) :-
 % S1 and S2 are connected if there is a path from S1->S2 or S2->S1
 connect(S1, S2) :-
     path(S1, S2).
-connect(S1, S2) :-
-    path(S2, S1).
+% connect(S1, S2) :-
+%     path(S2, S1).
 
 % check if a state has been visited
-notYetVisited(State, PathSoFar) :-
-    sort(State, SortedState),
-    \+ member(SortedState, PathSoFar).
-
 % notYetVisited(State, PathSoFar) :-
 %     permutation(State, PermuteState),
 %     not(member(PermuteState, PathSoFar)).
+notYetVisited(State, PathSoFar) :-
+    \+ memberchk(State, PathSoFar). % Use memberchk for more efficient
 
 % depthFirst(Start, Path, PathSoFar) returns the Path from start
 % to goal state given the path so far
 depthFirst(X, [X], _) :-
-    goal(X). % if X is the goal, return X
-
+    goal(GoalState), % if X is the goal, return X
+    permutation(GoalState, X),
+    writeln('Finish!'),
+    !.
+    
 % else, expand X by Y and find path from Y
 depthFirst(X, [X|Ypath], VISITED) :-
     connect(X, Y), % find a path from X to Y
@@ -85,42 +102,58 @@ depthFirst(X, [X|Ypath], VISITED) :-
 
 %---------------------------------------------------------------------------------
 % Test cases
-% Test case 1: Default start and goal states
 test_case(1,
     [[on, a, b], [on, b, 'table'], [on, c, d], [clear, c], [clear, a], [on, d, 'table']],
     [[on, d, a], [on, a, c], [on, c, b], [on, b, 'table'], [clear, a]]).
-
-% Test case 2: Move from table onto block
 test_case(2,
     [[on, a, 'table'], [on, b, 'table'], [clear, a], [clear, b]],
     [[on, a, b], [on, b, 'table'], [clear, a]]).
-
-% Test case 3: 
 test_case(3,
     [[on, a, b], [on, b, 'table'], [on, c, 'table'], [clear, a], [clear, c]],
     [[on, c, a], [on, a, b], [on, b, 'table'], [clear, c]]).
-
-% Test case 4: Move multiple blocks onto table and back
 test_case(4,
     [[on, a, b], [on, b, c], [on, c, 'table'], [clear, a]],
     [[on, b, 'table'], [on, c, a], [on, a, 'table'], [clear, c], [clear, b]]).
-
+test_case(5,
+    [[on, a, d], [on, d, b],  [on, b, 'table'], [clear, a], [on, c, 'table'], [clear, c]],
+    [[on, c, a], [on, a, b], [on, b, d], [on, d, 'table'], [clear, c]]).
 
 % Run single test case
 run_test(TestID) :-
     test_case(TestID, StartState, GoalState),
+    retractall(start(_)),
+    retractall(goal(_)),
+    assert(start(StartState)),
+    assert(goal(GoalState)),
+    format('-------------------------------------------------~n', []),
     format('Running Test Case ~w~n', [TestID]),
+    format('-------------------------------------------------~n', []),
     format('Start State: ~w~n', [StartState]),
     format('Goal State: ~w~n', [GoalState]),
     statistics(runtime, [StartTime|_]),
+    writeln('Processing...'),
     (depthFirst(StartState, Path, []) ->
         statistics(runtime, [EndTime|_]),
         Time is EndTime - StartTime,
-        format('Path: ~w~n', [Path]),
-        format('Time taken: ~w ms~n~n', [Time])
+        format('Path: ~n'),
+        print_path(Path),
+        format('-------------------------------------------------~n', []),
+        format('Execution Time: ~w ms~n', [Time])
     ;
-        format('No path found!~n~n')
-    ).
+        format('No path found!~n')
+    ),
+    retractall(start(_)),
+    retractall(goal(_)),
+    format('-------------------------------------------------~n', []).
+
+% Helper functions to print the path in better and more readable format
+print_path([]).
+print_path([State]) :- 
+    format('Final State: ~w~n', [State]).
+print_path([_, State|Rest]) :-
+    format('~w~n', [State]),
+    print_path([State|Rest]).
+
 
 % Run all test cases
 run_all :-
@@ -131,5 +164,3 @@ run_all :-
     % run_test(4),
     forall(test_case(ID, _, _), run_test(ID)),
     write('All test cases passed!'), nl.
-
-:- run_test(1).
